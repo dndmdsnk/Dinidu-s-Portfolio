6d6b2b7990a9d7ef127f87d646d7c30b9f97bd45
dndmdsnk <dndmdsnk@gmail.com>
2025-10-15 05:00:13 +0530

    done

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 export { Sky } from "./Sky";
 export { Bird } from './Bird'
 export { Plane } from "./Plane";
-export { Island } from "./Island";
+export { Island } from "./island";
 export { Fox } from "./Fox"
